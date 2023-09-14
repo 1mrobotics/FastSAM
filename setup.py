56cea871649748b72e91992e8e6311dd9d3091d7
Onesh <onesh@1mrobotics.com>
2023-09-14 12:02:54 +0300

    setup.py: pull deps from requirements.txt

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
+from pkg_resources import parse_requirements
 from setuptools import find_packages, setup
+from pathlib import Path
 
 setup(
     name="fastsam",
     version="0.1.1",
-    install_requires=[],
+    install_requires=list(map(str, parse_requirements((Path(__file__).parent / 'requirements.txt').read_text()))),
     package_dir= {
         "fastsam": "fastsam",
         "fastsam_tools": "utils",
